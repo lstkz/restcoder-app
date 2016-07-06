c29116c2f9226f16a973536c07640875634013a1
sky <sky@Mac-Pro-sky.local>
2016-07-06 17:32:39 +0200

    new landing page

--- src/containers/NewLanding/NewLanding.js
+++ src/containers/NewLanding/NewLanding.js
@@ -1,17 +1,24 @@
-import React, { PropTypes } from 'react';
+import React, {PropTypes} from 'react';
+import Helmet from 'react-helmet';
+import config from '../../config';
+import {asyncConnect} from 'redux-async-connect';
 import styles from './NewLanding.scss';
 import {Navbar} from 'react-bootstrap';
 import classNames from 'classnames';
 
 
+@asyncConnect([{
+  promise: () => {
+    return Promise.resolve();
+  }
+}])
 export default class NewLanding extends React.Component {
-  static propTypes = {
-
-  };
+  static propTypes = {};
 
   render() {
     return (
       <div className={styles.NewLanding}>
+        <Helmet {...config.app.head} title="RestCoder" titleTemplate="%s" />
         <header>
           <div className="container">
 
@@ -19,7 +26,7 @@ export default class NewLanding extends React.Component {
               <Navbar.Header>
                 <Navbar.Brand>
                   <a className="brand" href="#">
-                    <img width="220" height="37" src={require('./images/logo.png')} />
+                    <img width="220" height="37" src={require('./images/logo.png')}/>
                   </a>
                 </Navbar.Brand>
                 <Navbar.Toggle />
@@ -32,16 +39,6 @@ export default class NewLanding extends React.Component {
 
               </Navbar.Collapse>
             </Navbar>
-
-            {/*<nav className="navbar" role="navigation">
-              <div className="navbar-header pull-left">
-                <a className="brand" href="#">
-                  <img width="220" height="37" src={require('./images/logo.png')} />
-                </a>
-              </div>
-              <form className="navbar-form navbar-right pull-right">
-              </form>
-            </nav>*/}
           </div>
         </header>
         <section className={styles.cover}>
@@ -51,18 +48,18 @@ export default class NewLanding extends React.Component {
             And the best way to practice coding is to code!
           </h2>
           <div className="mth">
-              <a className={classNames(styles.socialBtn, styles.socialBtnFB)}>
-                <i className="fa fa-facebook"/>
-                <span>Start with Facebook</span>
-              </a>
-              <a className={classNames(styles.socialBtn, styles.socialBtnG)}>
-                <i className="fa fa-google-plus"/>
-                <span>Start with Google</span>
-              </a>
-              <a className={classNames(styles.socialBtn, styles.socialBtnGH)}>
-                <i className="fa fa-github"/>
-                <span>Start with Github</span>
-              </a>
+            <a className={classNames(styles.socialBtn, styles.socialBtnFB)}>
+              <i className="fa fa-facebook"/>
+              <span>Start with Facebook</span>
+            </a>
+            <a className={classNames(styles.socialBtn, styles.socialBtnG)}>
+              <i className="fa fa-google-plus"/>
+              <span>Start with Google</span>
+            </a>
+            <a className={classNames(styles.socialBtn, styles.socialBtnGH)}>
+              <i className="fa fa-github"/>
+              <span>Start with Github</span>
+            </a>
           </div>
         </section>
 
@@ -96,7 +93,7 @@ export default class NewLanding extends React.Component {
               <div className="row">
                 <div className="col-md-6 col-xs-12">
                   <div className={styles.infoItem}>
-                    <i className={styles.iconAtom} />
+                    <i className={styles.iconAtom}/>
                     <span>
                       Create professional applications using <strong>modern technologies</strong>
                     </span>
@@ -106,7 +103,7 @@ export default class NewLanding extends React.Component {
               <div className="row">
                 <div className="col-md-6 col-xs-12">
                   <div className={styles.infoItem}>
-                    <i className={styles.iconPentagram} />
+                    <i className={styles.iconPentagram}/>
                     <span>
                       Choose Practice Mode or Competition Mode for <strong>flexibility that suits you</strong>
                     </span>
@@ -116,7 +113,7 @@ export default class NewLanding extends React.Component {
               <div className="row">
                 <div className="col-md-6 col-xs-12">
                   <div className={styles.infoItem}>
-                    <i className={styles.iconFinger} />
+                    <i className={styles.iconFinger}/>
                     <span>
                       <strong>Master integration</strong> with 3rd party database or APIs
                     </span>
@@ -126,7 +123,7 @@ export default class NewLanding extends React.Component {
               <div className="row">
                 <div className="col-md-6 col-xs-12">
                   <div className={styles.infoItem}>
-                    <i className={styles.iconCode} />
+                    <i className={styles.iconCode}/>
                     <span>
                       RestCoder automatically <strong>verifies your solutions</strong> to help your learn
                     </span>
@@ -137,6 +134,133 @@ export default class NewLanding extends React.Component {
                 <div className="col-md-6 col-xs-12">
                   <div className={styles.infoItem}>
                     <button className="btn btn-lg btn-primary">Try it now</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.become}>
+          <div className="container">
+            <h1>Become a better backend developer!</h1>
+            <h2>Our API challenges improve your skills</h2>
+            <div className="row">
+              <div className="col-md-4">
+                <i className={styles.iconChallenge}/>
+                <h3>Pick a challenge</h3>
+                <p>
+                  Sharpen your skills, with >10 challenges to solve and more on the way.
+
+                </p>
+              </div>
+              <div className="col-md-4 col-md-offset-0">
+                <i className={styles.iconKeyboard}/>
+                <h3>Code</h3>
+                <p>
+                  Choose your favorite language and technology. Build a RESTful API application.
+                </p>
+              </div>
+              <div className="col-md-4 col-md-offset-0">
+                <i className={styles.iconSubmit}/>
+                <h3>Submit</h3>
+                <p>
+                  Our tester automatically verifies your solutions to help you learn!
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.familiar}>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-6">
+                <p>
+                  If you are familiar with competitive programming, but are into software development rather than algorithms, RestCoder is the platform you have been waiting for.
+                </p>
+              </div>
+              <div className="col-sm-4 col-sm-offset-2">
+                <i className={styles.iconHipster}/>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.tech}>
+          <div className={styles.techRowWrapper}>
+            <div className="container">
+              <div className={classNames('row', styles.techRow)}>
+                <div className="col-md-3">
+                  <h3>Powered by:</h3>
+                </div>
+                <div className="col-md-9">
+                  <div className={styles.techInnerRow}>
+                    <i className={styles.iconC9}/>
+                    <i className={styles.iconPostman}/>
+                    <i className={styles.iconDocker}/>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.techRowWrapper}>
+            <div className="container">
+              <div className={classNames('row', styles.techRow)}>
+                <div className="col-md-3 col-sm-12">
+                  <h3>Languages:</h3>
+                </div>
+                <div className="col-md-9 col-sm-12">
+                  <div className={styles.techInnerRow}>
+                    <div className={styles.lang}>
+                      <i className={styles.iconJs}/>
+                      <span>nodejs</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconRuby}/>
+                      <span>ruby</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconPython}/>
+                      <span>python</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconJava}/>
+                      <span>java</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconDotnet}/>
+                      <span>.net</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.techRowWrapper}>
+            <div className="container">
+              <div className={classNames('row', styles.techRow)}>
+                <div className="col-md-3">
+                  <h3>Coming soon:</h3>
+                </div>
+                <div className="col-md-9">
+                  <div className={styles.techInnerRow}>
+                    <div className={styles.lang}>
+                      <i className={styles.iconPhp}/>
+                      <span>php</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconGo}/>
+                      <span>go</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconClojure}/>
+                      <span>clojure</span>
+                    </div>
+                    <div className={styles.lang}>
+                      <i className={styles.iconScala}/>
+                      <span>scala</span>
+                    </div>
                   </div>
                 </div>
               </div>
